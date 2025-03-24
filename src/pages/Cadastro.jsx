01aa8fe060f4c446c89c72ef88aa84bd89f19210
Hítalon Saimon <147450576+H-Saimon@users.noreply.github.com>
2025-03-24 20:43:39 -0300

    Update Cadastro.jsx

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -6,22 +6,22 @@ function Cadastro() {
   return (
     <Box 
     sx={{
-      textAlign: "center",
-      p: 0,
-      backgroundImage: `url(${background})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundColor: "#f0f0f0",
-      minHeight: "100vh", // Garante que cubra toda a altura da tela
-      width: "100vw", // Garante que cubra toda a largura da tela
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center"
+        textAlign: "center",
+        p: 0,
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "#f0f0f0",
+        minHeight: "100vh", // Garante que cubra toda a altura da tela
+        width: "100vw", // Garante que cubra toda a largura da tela
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
     }}
     >
-    <FormCadastro/>
+      <FormCadastro/>
     </Box>
   );
 }
